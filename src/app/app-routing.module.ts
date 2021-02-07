809c1950d2049ccc001187b4964019681eb648e9
Lukasz Kadracki <lkadracki@gmail.com>
2021-02-07 17:31:05 +0100

    szkieleton

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,38 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import {AdminComponent} from './layout/admin/admin.component';
+import {AuthComponent} from './layout/auth/auth.component';
+
+const routes: Routes = [
+  {
+    path: '',
+    component: AdminComponent,
+    children: [
+      {
+        path: '',
+        redirectTo: 'other/simple-page',
+        pathMatch: 'full'
+      },
+      {
+        path: 'other/simple-page',
+        loadChildren: () => import('./theme/simple-page/simple-page.module').then(m => m.SimplePageModule)
+      }
+    ]
+  },
+  {
+    path: '',
+    component: AuthComponent,
+    children: [
+    ]
+  },
+  {
+    path: '**',
+    redirectTo: 'home/dashboard/default'
+  }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
