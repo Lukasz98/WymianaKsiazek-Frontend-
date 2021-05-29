d464af4d117324908db3fe5609655e21117480ae
ambro-mlody <52577549+ambro-mlody@users.noreply.github.com>
2021-05-29 19:38:11 +0200

    img

--- src/app/_models/offer.ts
+++ src/app/_models/offer.ts
@@ -1,17 +1,17 @@
 import {Book} from "./book";
 import {Address} from "./address"
 export interface Offer {
-    imgSrc: string,
     id: number,
     content: string,
     createdOn: string,
     updatedOn: string,
+    type: boolean,
+    price: number,
     imgName1: string,
     imgName2: string,
     imgName3: string,
     icon: number,
-    type: boolean,
-    price: number,
+
     address: Address,
     book: Book,
     user: string
